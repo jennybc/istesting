253943b2f27ef2952d79fdcb8642893854ba90ce
Jenny Bryan <jenny.f.bryan@gmail.com>
2025-07-25 11:26:35 -0700

    Evalue is_testing() at top-level of helper file

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,3 +1,5 @@
 is_testing_report <- function() {
   is_testing()
 }
+
+expect_true(is_testing())
--- tests/testthat/test-aaa.R
+++ tests/testthat/test-aaa.R
@@ -10,4 +10,10 @@ test_that("is_testing() works in a actual package function", {
   expect_equal(foo(), "yo we're running tests")
 })
 
-expect_equal(is_testing())
+test_that("TESTTHAT env var is 'true'", {
+  expect_equal(Sys.getenv("TESTTHAT"), "true")
+})
+
+expect_equal(Sys.getenv("TESTTHAT"), "true")
+
+expect_true(is_testing())
